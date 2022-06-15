0b401b0467019a30c6932a9f7c72b789a18acd79
Matthieu Barba <mbarba@ebi.ac.uk>
2022-06-15 12:06:36 +0100

    clean up get missed issues script

--- lib/veupathdb/redmine/get_missed_issues.py
+++ lib/veupathdb/redmine/get_missed_issues.py
@@ -27,48 +27,66 @@ supported_team = "Data Processing (EBI)"
 supported_status_id = 20
 
 
-def get_missed_datasets(redmine) -> None:
+def print_issues(issues: list, description: str) -> None:
+    print(f"{len(issues)} issues for {description}")
+    for issue in issues:
+        print(IssueUtils.tostr_full(issue))
+
+
+def get_missed_datasets(redmine: VeupathRedmineClient) -> list:
     """Get issues assigned to the team, but not for a supported dataset"""
 
     redmine.add_filter("team", supported_team)
     all_issues = redmine.get_issues()
 
+    missed = []
     for issue in all_issues:
         cfs = IssueUtils.get_custom_fields(issue)
         if "DataType" in cfs:
             datatype = cfs["DataType"]
             if datatype not in supported_datasets:
-                print(f"No {datatype}? in {IssueUtils.tostr_full(issue)}")
+                missed.append(issue)
+    
+    redmine.remove_filter("team")
+    return missed
 
 
-def get_missed_status(redmine) -> None:
+def get_missed_status(redmine: VeupathRedmineClient) -> list:
     """Get issues with the right status, but not the right team"""
 
     redmine.add_filter("status_id", supported_status_id)
     all_issues = redmine.get_issues()
 
+    missed = []
     for issue in all_issues:
         cfs = IssueUtils.get_custom_fields(issue)
         if "VEuPathDB Team" in cfs:
             team = cfs["VEuPathDB Team"]
             if team != "Data Processing (EBI)":
-                print(f"For EBI team? {IssueUtils.tostr_full(issue)}")
+                missed.append(issue)
+    
+    redmine.remove_filter("status_id")
+    return missed
 
 
-def get_missed_assignee(redmine, user_id) -> None:
+def get_missed_assignee(redmine, user_id) -> list:
     """Get issues with the right user, but not assigned to the team"""
 
     redmine.add_filter("assigned_to_id", user_id)
     all_issues = redmine.get_issues()
 
+    missed = []
     for issue in all_issues:
         cfs = IssueUtils.get_custom_fields(issue)
         if not cfs:
-            print(f"Non standard? {IssueUtils.tostr_full(issue)}")
-        if "VEuPathDB Team" in cfs:
+            missed.append(issue)
+        elif "VEuPathDB Team" in cfs:
             team = cfs["VEuPathDB Team"]
             if team != "Data Processing (EBI)":
-                print(f"For EBI team? {IssueUtils.tostr_full(issue)}")
+                missed.append(issue)
+
+    redmine.remove_filter("assigned_to_id")
+    return missed
 
 
 def main():
@@ -100,24 +118,32 @@ def main():
         redmine.set_build(args.build)
 
     if args.get_missed == 'datasets':
-        get_missed_datasets(redmine)
+        issues = get_missed_datasets(redmine)
+        print_issues(issues, "missed datasets")
 
     elif args.get_missed == 'status':
-        get_missed_status(redmine)
+        issues = get_missed_status(redmine)
+        print_issues(issues, "missed status")
 
     elif args.get_missed == 'assignee':
         if not args.user_id:
-            print("User id required")
+            print("User id required for missed assignee")
             return
-        get_missed_assignee(redmine, args.user_id)
+        issues = get_missed_assignee(redmine, args.user_id)
+        print_issues(issues, "missed assignee")
 
     elif args.get_missed == 'all':
-        get_missed_datasets(redmine)
-        get_missed_status(redmine)
+        issues_ds = get_missed_datasets(redmine)
+        print_issues(issues_ds, "missed datasets")
+
+        issues_st = get_missed_status(redmine)
+        print_issues(issues_st, "missed status")
+
         if not args.user_id:
-            print("User id required")
+            print("User id required for missed assignee")
             return
-        get_missed_assignee(redmine, args.user_id)
+        issues_user = get_missed_assignee(redmine, args.user_id)
+        print_issues(issues_user, "missed assignee")
 
 
 if __name__ == "__main__":
