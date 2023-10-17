dd5325a4ec915cb48fa832c04f9ea51af5ea4ac3
Jorge Alvarez Jarreta <jalvarez@ebi.ac.uk>
2023-10-17 14:29:08 +0100

    bugfix: the first ID in the list may not be the correct one

--- src/veupath/redmine/client/genome.py
+++ src/veupath/redmine/client/genome.py
@@ -223,17 +223,19 @@ class Genome(RedmineIssue):
             except ValidationError:
                 self.errors("Validation error")
                 return summary
-            ids = record["IdList"]
+            id_list = record["IdList"]
 
-            if len(ids) == 0:
+            if len(id_list) == 0:
                 self.add_error("Assembly not found in INSDC")
             else:
-                if len(ids) > 1:
-                    print(f"{len(ids)} assemblies found for {self.accession}, using the first one ({ids[0]})")
-                id = ids[0]
-                summary_full = self.get_assembly_metadata(id)
-                summary = summary_full["DocumentSummarySet"]["DocumentSummary"][0]
-                self.insdc_metadata = summary
+                for accession_id in id_list:
+                    summary_full = self.get_assembly_metadata(accession_id)
+                    summary = summary_full["DocumentSummarySet"]["DocumentSummary"][0]
+                    if summary["AssemblyAccession"] == self.accession:
+                        if len(id_list) > 1:
+                            print(f"{self.accession} matched {len(id_list)} assemblies, using {accession_id}")
+                        self.insdc_metadata = summary
+                        break
         return summary
 
     def get_assembly_metadata(self, id):
